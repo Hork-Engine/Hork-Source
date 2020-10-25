230d5b6b09120209939b85ad978c699cd8e89254
Alexander Samusev <alsamusev@list.ru>
2020-10-26 00:24:59 +0300

    Quaternion normalization in STransform

--- Source/Engine/Core/Public/Transform.h
+++ Source/Engine/Core/Public/Transform.h
@@ -295,6 +295,7 @@ AN_FORCEINLINE void STransform::TurnAroundAxis( float _DeltaAngleRad, Float3 con
     Math::SinCos( _DeltaAngleRad * 0.5f, s, c );
 
     Rotation = Quat( c, s * _NormalizedAxis.X, s * _NormalizedAxis.Y, s * _NormalizedAxis.Z ) * Rotation;
+    Rotation.NormalizeSelf();
 }
 
 AN_FORCEINLINE void STransform::TurnAroundVector( float _DeltaAngleRad, Float3 const & _Vector ) {
