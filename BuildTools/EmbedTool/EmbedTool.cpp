7da072a41a484936a04d11b4f3ae668b758b0108
Alexander Samusev <alsamusev@list.ru>
2024-07-07 23:34:45 +0300

    Code formatting

--- BuildTools/EmbedTool/EmbedTool.cpp
+++ BuildTools/EmbedTool/EmbedTool.cpp
@@ -41,49 +41,57 @@ Resource embedding tool program
 
 using byte = uint8_t;
 
-using ReadDirCallback = std::function< void( std::string const & FileName, bool bIsDirectory ) >;
+using ReadDirCallback = std::function<void(std::string const& FileName, bool bIsDirectory)>;
 
 #ifdef __linux__
 
-#include <dirent.h>
-#include <sys/stat.h>
-#include <functional>
+#    include <dirent.h>
+#    include <sys/stat.h>
+#    include <functional>
 
-void ReadDir( std::string const & Path, bool bSubDirs, ReadDirCallback Callback )
+void ReadDir(std::string const& Path, bool bSubDirs, ReadDirCallback Callback)
 {
     std::string fn;
-    DIR * dir = opendir( Path.c_str() );
-    if ( dir ) {
-        while ( 1 ) {
-            dirent * entry = readdir( dir );
-            if ( !entry ) {
+    DIR* dir = opendir(Path.c_str());
+    if (dir)
+    {
+        while (1)
+        {
+            dirent* entry = readdir(dir);
+            if (!entry)
+            {
                 break;
             }
 
             fn = Path;
             int len = Path.length();
-            if ( len > 1 && Path[len-1] != '/' ) {
+            if (len > 1 && Path[len - 1] != '/')
+            {
                 fn += "/";
             }
             fn += entry->d_name;
 
             struct stat statInfo;
-            lstat( fn.c_str(), &statInfo );
+            lstat(fn.c_str(), &statInfo);
 
-            if ( S_ISDIR( statInfo.st_mode ) ) {
-                if ( bSubDirs ) {
-                    if ( !strcmp( ".", entry->d_name )
-                         || !strcmp( "..", entry->d_name ) ) {
+            if (S_ISDIR(statInfo.st_mode))
+            {
+                if (bSubDirs)
+                {
+                    if (!strcmp(".", entry->d_name) || !strcmp("..", entry->d_name))
+                    {
                         continue;
                     }
-                    ReadDir( fn.c_str(), bSubDirs, Callback );
+                    ReadDir(fn.c_str(), bSubDirs, Callback);
                 }
-                Callback( fn, true );
-            } else {
-                Callback( Path + "/" + entry->d_name, false );
+                Callback(fn, true);
+            }
+            else
+            {
+                Callback(Path + "/" + entry->d_name, false);
             }
         }
-        closedir( dir );
+        closedir(dir);
     }
 }
 
@@ -91,203 +99,237 @@ void ReadDir( std::string const & Path, bool bSubDirs, ReadDirCallback Callback 
 
 #ifdef _WIN32
 
-#include <Windows.h>
+#    include <Windows.h>
 
-#pragma warning( disable : 4996 )
+#    pragma warning(disable : 4996)
 
-void ReadDir( std::string const & Path, bool bSubDirs, ReadDirCallback Callback )
+void ReadDir(std::string const& Path, bool bSubDirs, ReadDirCallback Callback)
 {
     std::string fn;
 
     HANDLE fh;
     WIN32_FIND_DATAA fd;
 
-    if ( (fh = FindFirstFileA( (Path +  "/*.*").c_str(), &fd )) != INVALID_HANDLE_VALUE ) {
+    if ((fh = FindFirstFileA((Path + "/*.*").c_str(), &fd)) != INVALID_HANDLE_VALUE)
+    {
         do {
             fn = Path;
             int len = (int)Path.length();
-            if ( len > 1 && Path[len-1] != '/' ) {
+            if (len > 1 && Path[len - 1] != '/')
+            {
                 fn += "/";
             }
             fn += fd.cFileName;
 
-            if ( fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY ) {
-                if ( bSubDirs ) {
-                    if ( !strcmp( ".", fd.cFileName )
-                         || !strcmp( "..", fd.cFileName ) ) {
+            if (fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+            {
+                if (bSubDirs)
+                {
+                    if (!strcmp(".", fd.cFileName) || !strcmp("..", fd.cFileName))
+                    {
                         continue;
                     }
-                    ReadDir( fn.c_str(), bSubDirs, Callback );
+                    ReadDir(fn.c_str(), bSubDirs, Callback);
                 }
-                Callback( fn, true );
-            } else {
-                Callback( Path + "/" + fd.cFileName, false );
+                Callback(fn, true);
             }
-        } while ( FindNextFileA( fh, &fd ) );
+            else
+            {
+                Callback(Path + "/" + fd.cFileName, false);
+            }
+        } while (FindNextFileA(fh, &fd));
 
-        FindClose( fh );
+        FindClose(fh);
     }
 }
 
 #endif
 
-void FixSeparator( std::string & s ) {
-    std::replace( s.begin(), s.end(), '\\', '/' );
+void FixSeparator(std::string& s)
+{
+    std::replace(s.begin(), s.end(), '\\', '/');
 }
 
-bool IsPathSeparator( char _Char ) {
+bool IsPathSeparator(char _Char)
+{
     return _Char == '/' || _Char == '\\';
 }
 
-void ClipFilename( std::string & s ) {
+void ClipFilename(std::string& s)
+{
     int len = (int)s.length();
-    while ( --len > 0 && !IsPathSeparator( s[len] ) ) {
+    while (--len > 0 && !IsPathSeparator(s[len]))
+    {
         ;
     }
-    s.resize( len );
+    s.resize(len);
 }
 
-constexpr int32_t INT64_HIGH_INT( uint64_t i64 ) {
+constexpr int32_t INT64_HIGH_INT(uint64_t i64)
+{
     return i64 >> 32;
 }
 
-constexpr int32_t INT64_LOW_INT( uint64_t i64 ) {
+constexpr int32_t INT64_LOW_INT(uint64_t i64)
+{
     return i64 & 0xFFFFFFFF;
 }
 
-constexpr size_t Align( size_t N, size_t Alignment ) {
-    return ( N + ( Alignment - 1 ) ) & ~( Alignment - 1 );
+constexpr size_t Align(size_t N, size_t Alignment)
+{
+    return (N + (Alignment - 1)) & ~(Alignment - 1);
 }
 
-void WriteBinaryToC( FILE * Stream, const char * SymName, const void * pData, size_t SizeInBytes, bool bEncodeBase85 )
+void WriteBinaryToC(FILE* Stream, const char* SymName, const void* pData, size_t SizeInBytes, bool bEncodeBase85)
 {
-    const byte * bytes = (const byte *)pData;
+    const byte* bytes = (const byte*)pData;
 
-    fprintf( Stream, "#include <stdio.h>\n" );
-    fprintf( Stream, "#include <stdint.h>\n" );
+    fprintf(Stream, "#include <stdio.h>\n");
+    fprintf(Stream, "#include <stdint.h>\n");
 
-    if ( bEncodeBase85 ) {
-        fprintf( Stream, "const char %s_Data_Base85[%d+1] =\n    \"", SymName, (int)((SizeInBytes+3)/4)*5 );
+    if (bEncodeBase85)
+    {
+        fprintf(Stream, "const char %s_Data_Base85[%d+1] =\n    \"", SymName, (int)((SizeInBytes + 3) / 4) * 5);
         char prev_c = 0;
-        for ( size_t i = 0; i < SizeInBytes; i += 4 ) {
-            uint32_t d = *(uint32_t *)(bytes + i);
-            for ( int j = 0; j < 5; j++, d /= 85 ) {
+        for (size_t i = 0; i < SizeInBytes; i += 4)
+        {
+            uint32_t d = *(uint32_t*)(bytes + i);
+            for (int j = 0; j < 5; j++, d /= 85)
+            {
                 unsigned int x = (d % 85) + 35;
                 char c = (x >= '\\') ? x + 1 : x;
-                fprintf( Stream, (c == '?' && prev_c == '?') ? "\\%c" : "%c", c );
+                fprintf(Stream, (c == '?' && prev_c == '?') ? "\\%c" : "%c", c);
                 prev_c = c;
             }
-            if ( (i % 112) == 112-4 )
-                fprintf( Stream, "\"\n    \"" );
+            if ((i % 112) == 112 - 4)
+                fprintf(Stream, "\"\n    \"");
         }
-        fprintf( Stream, "\";\n\n" );
-    } else {
-        fprintf( Stream, "const size_t %s_Size = %d;\n", SymName, (int)SizeInBytes );
-        fprintf( Stream, "const uint64_t %s_Data[%d] =\n{", SymName, (int)Align( SizeInBytes, 8 ) );
+        fprintf(Stream, "\";\n\n");
+    }
+    else
+    {
+        fprintf(Stream, "const size_t %s_Size = %d;\n", SymName, (int)SizeInBytes);
+        fprintf(Stream, "const uint64_t %s_Data[%d] =\n{", SymName, (int)Align(SizeInBytes, 8));
         int column = 0;
-        for ( size_t i = 0; i < SizeInBytes; i += 8 ) {
-            uint64_t d = *(uint64_t *)(bytes + i);
-            if ( (column++ % 6) == 0 ) {
-                fprintf( Stream, "\n    0x%08x%08x", INT64_HIGH_INT( d ), INT64_LOW_INT( d ) );
-            } else {
-                fprintf( Stream, "0x%08x%08x", INT64_HIGH_INT( d ), INT64_LOW_INT( d ) );
+        for (size_t i = 0; i < SizeInBytes; i += 8)
+        {
+            uint64_t d = *(uint64_t*)(bytes + i);
+            if ((column++ % 6) == 0)
+            {
+                fprintf(Stream, "\n    0x%08x%08x", INT64_HIGH_INT(d), INT64_LOW_INT(d));
             }
-            if ( i + 8 < SizeInBytes ) {
-                fprintf( Stream, ", " );
+            else
+            {
+                fprintf(Stream, "0x%08x%08x", INT64_HIGH_INT(d), INT64_LOW_INT(d));
+            }
+            if (i + 8 < SizeInBytes)
+            {
+                fprintf(Stream, ", ");
             }
         }
-        fprintf( Stream, "\n};\n\n" );
+        fprintf(Stream, "\n};\n\n");
     }
 }
 
-int GenerateEmbeddedResources( const char * SourcePath, const char * ResultFile )
+int GenerateEmbeddedResources(const char* SourcePath, const char* ResultFile)
 {
     std::string path = SourcePath;
-    FixSeparator( path );
+    FixSeparator(path);
 
-    void * pBuf = nullptr;
+    void* pBuf = nullptr;
     size_t size = 0;
 
     mz_zip_archive zip;
-    memset( &zip, 0, sizeof( zip ) );
+    memset(&zip, 0, sizeof(zip));
 
-    printf( "Source '%s'\n"
-            "Destination: '%s'\n", SourcePath, ResultFile );
+    printf("Source '%s'\n"
+           "Destination: '%s'\n",
+           SourcePath, ResultFile);
 
-    if ( mz_zip_writer_init_heap_v2( &zip, 0, 0, 0 ) ) {
-        ReadDir( path, true,
-                 [&zip, &path]( std::string const & FileName, bool bIsDirectory )
-        {
-            if ( !bIsDirectory ) {
-                printf( "Embedding '%s'\n", &FileName[path.length()] );
+    if (mz_zip_writer_init_heap_v2(&zip, 0, 0, 0))
+    {
+        ReadDir(path, true,
+                [&zip, &path](std::string const& FileName, bool bIsDirectory)
+                {
+                    if (!bIsDirectory)
+                    {
+                        printf("Embedding '%s'\n", &FileName[path.length()]);
 
-                mz_bool status = mz_zip_writer_add_file( &zip, &FileName[path.length()], FileName.c_str(), nullptr, 0, MZ_UBER_COMPRESSION );
-                if ( !status ) {
-                    printf( "Failed to zip %s\n", FileName.c_str() );
-                }
-            }
-        } );
+                        mz_bool status = mz_zip_writer_add_file(&zip, &FileName[path.length()], FileName.c_str(), nullptr, 0, MZ_UBER_COMPRESSION);
+                        if (!status)
+                        {
+                            printf("Failed to zip %s\n", FileName.c_str());
+                        }
+                    }
+                });
 
-        mz_zip_writer_finalize_heap_archive( &zip, &pBuf, &size );
-        mz_zip_writer_end( &zip );
+        mz_zip_writer_finalize_heap_archive(&zip, &pBuf, &size);
+        mz_zip_writer_end(&zip);
     }
 
-    if ( pBuf ) {
+    if (pBuf)
+    {
         path = ResultFile;
-        FixSeparator( path );
+        FixSeparator(path);
 
-        FILE * f;
-        f = fopen( path.c_str(), "w" );
-        if ( f ) {
-            WriteBinaryToC( f, "EmbeddedResources", pBuf, size, false );
-            fclose( f );
+        FILE* f;
+        f = fopen(path.c_str(), "w");
+        if (f)
+        {
+            WriteBinaryToC(f, "EmbeddedResources", pBuf, size, false);
+            fclose(f);
         }
 
-        mz_free( pBuf );
+        mz_free(pBuf);
     }
 
     return 0;
 }
 
-int GenerateEmbeddedResourcesZip( const char * SourcePath, const char * ResultFile )
+int GenerateEmbeddedResourcesZip(const char* SourcePath, const char* ResultFile)
 {
     std::string path = SourcePath;
-    FixSeparator( path );
+    FixSeparator(path);
 
     mz_zip_archive zip;
-    memset( &zip, 0, sizeof( zip ) );
+    memset(&zip, 0, sizeof(zip));
 
-    printf( "Source '%s'\n"
-            "Destination: '%s'\n", SourcePath, ResultFile );
+    printf("Source '%s'\n"
+           "Destination: '%s'\n",
+           SourcePath, ResultFile);
 
-    if ( mz_zip_writer_init_file( &zip, ResultFile, 0 ) ) {
-        ReadDir( path, true,
-                 [&zip, &path]( std::string const & FileName, bool bIsDirectory )
-        {
-            if ( !bIsDirectory ) {
-                printf( "Embedding '%s'\n", &FileName[path.length()] );
+    if (mz_zip_writer_init_file(&zip, ResultFile, 0))
+    {
+        ReadDir(path, true,
+                [&zip, &path](std::string const& FileName, bool bIsDirectory)
+                {
+                    if (!bIsDirectory)
+                    {
+                        printf("Embedding '%s'\n", &FileName[path.length()]);
 
-                mz_bool status = mz_zip_writer_add_file( &zip, &FileName[path.length()], FileName.c_str(), nullptr, 0, MZ_UBER_COMPRESSION );
-                if ( !status ) {
-                    printf( "Failed to zip %s\n", FileName.c_str() );
-                }
-            }
-        } );
+                        mz_bool status = mz_zip_writer_add_file(&zip, &FileName[path.length()], FileName.c_str(), nullptr, 0, MZ_UBER_COMPRESSION);
+                        if (!status)
+                        {
+                            printf("Failed to zip %s\n", FileName.c_str());
+                        }
+                    }
+                });
 
-        mz_zip_writer_finalize_archive( &zip );
-        mz_zip_writer_end( &zip );
+        mz_zip_writer_finalize_archive(&zip);
+        mz_zip_writer_end(&zip);
     }
 
     return 0;
 }
 
-int main( int argc, char *argv[] ) {
-    printf( "Start embedding\n" );
-    if ( argc < 3 ) {
-        printf( "Not enough command line parameters\n" );
+int main(int argc, char* argv[])
+{
+    printf("Start embedding\n");
+    if (argc < 3)
+    {
+        printf("Not enough command line parameters\n");
         return -1;
     }
     //GenerateEmbeddedResourcesZip( argv[1], (std::string(argv[2])+".zip").c_str() );
-    return GenerateEmbeddedResources( argv[1], argv[2] );
+    return GenerateEmbeddedResources(argv[1], argv[2]);
 }
-
--- Engine/Geometry/BV/BvhTree.h
+++ Engine/Geometry/BV/BvhTree.h
@@ -42,28 +42,14 @@ BvhNode
 */
 struct BvhNode
 {
-    BvAxisAlignedBox Bounds;
-    int32_t          Index; // First primitive in leaf (Index >= 0), next node index (Index < 0)
-    int32_t          PrimitiveCount;
+    BvAxisAlignedBox        Bounds;
+    int32_t                 Index; // First primitive in leaf (Index >= 0), next node index (Index < 0)
+    int32_t                 PrimitiveCount;
 
-    bool IsLeaf() const
-    {
-        return Index >= 0;
-    }
+    bool                    IsLeaf() const;
 
-    void Read(IBinaryStreamReadInterface& Stream)
-    {
-        Stream.ReadObject(Bounds);
-        Index          = Stream.ReadInt32();
-        PrimitiveCount = Stream.ReadInt32();
-    }
-
-    void Write(IBinaryStreamWriteInterface& Stream) const
-    {
-        Stream.WriteObject(Bounds);
-        Stream.WriteInt32(Index);
-        Stream.WriteInt32(PrimitiveCount);
-    }
+    void                    Read(IBinaryStreamReadInterface& Stream);
+    void                    Write(IBinaryStreamWriteInterface& Stream) const;
 };
 
 /**
@@ -73,59 +59,79 @@ BvhTree
 Binary AABB-based BVH tree
 
 */
-class BvhTree : public Noncopyable
+class BvhTree final : public Noncopyable
 {
 public:
-    BvhTree();
-    virtual ~BvhTree() = default;
+                            BvhTree();
 
-    BvhTree(BvhTree&& Rhs) noexcept :
-        m_Nodes(std::move(Rhs.m_Nodes)),
-        m_Indirection(std::move(Rhs.m_Indirection)),
-        m_BoundingBox(Rhs.m_BoundingBox)
-    {
-        Rhs.m_BoundingBox.Clear();
-    }
+                            BvhTree(BvhTree&& Rhs) noexcept;
+                            BvhTree& operator=(BvhTree&& Rhs) noexcept;
 
-    BvhTree& operator=(BvhTree&& Rhs) noexcept
-    {
-        m_Nodes = std::move(Rhs.m_Nodes);
-        m_Indirection = std::move(Rhs.m_Indirection);
-        m_BoundingBox = Rhs.m_BoundingBox;
-        Rhs.m_BoundingBox.Clear();
-        return *this;
-    }
+                            template <typename VertexType>
+                            BvhTree(ArrayView<VertexType> Vertices, ArrayView<unsigned int> Indices, int BaseVertex, unsigned int PrimitivesPerLeaf);
 
-    template <typename VertexType>
-    BvhTree(ArrayView<VertexType> Vertices, ArrayView<unsigned int> Indices, int BaseVertex, unsigned int PrimitivesPerLeaf) :
-        BvhTree(&Vertices[0].Position, Vertices.Size(), sizeof(VertexType), Indices, BaseVertex, PrimitivesPerLeaf)
-    {}
+    int                     MarkRayOverlappingLeafs(Float3 const& RayStart, Float3 const& RayEnd, unsigned int* MarkLeafs, int MaxLeafs) const;
 
-    #if 0
-    BvhTree(ArrayView<PrimitiveDef> Primitives, unsigned int PrimitivesPerLeaf);
-    #endif
+    int                     MarkBoxOverlappingLeafs(BvAxisAlignedBox const& Bounds, unsigned int* MarkLeafs, int MaxLeafs) const;
 
-    int MarkRayOverlappingLeafs(Float3 const& RayStart, Float3 const& RayEnd, unsigned int* MarkLeafs, int MaxLeafs) const;
+    Vector<BvhNode> const&  GetNodes() const { return m_Nodes; }
 
-    int MarkBoxOverlappingLeafs(BvAxisAlignedBox const& Bounds, unsigned int* MarkLeafs, int MaxLeafs) const;
-
-    Vector<BvhNode> const& GetNodes() const { return m_Nodes; }
-
-    unsigned int const* GetIndirection() const { return m_Indirection.ToPtr(); }
+    unsigned int const*     GetIndirection() const { return m_Indirection.ToPtr(); }
 
     BvAxisAlignedBox const& GetBoundingBox() const { return m_BoundingBox; }
 
-    void Read(IBinaryStreamReadInterface& Stream);
-    void Write(IBinaryStreamWriteInterface& Stream) const;
+    void                    Read(IBinaryStreamReadInterface& Stream);
+    void                    Write(IBinaryStreamWriteInterface& Stream) const;
 
 private:
-    BvhTree(Float3 const* Vertices, size_t NumVertices, size_t VertexStride, ArrayView<unsigned int> Indices, int BaseVertex, unsigned int PrimitivesPerLeaf);
+                            BvhTree(Float3 const* Vertices, size_t NumVertices, size_t VertexStride, ArrayView<unsigned int> Indices, int BaseVertex, unsigned int PrimitivesPerLeaf);
 
-    void Subdivide(struct BvhBuildContext& Build, int Axis, int FirstPrimitive, int LastPrimitive, unsigned int PrimitivesPerLeaf, int& PrimitiveIndex);
+    void                    Subdivide(struct BvhBuildContext& Build, int Axis, int FirstPrimitive, int LastPrimitive, unsigned int PrimitivesPerLeaf, int& PrimitiveIndex);
 
-    Vector<BvhNode>      m_Nodes;
-    Vector<unsigned int> m_Indirection;
-    BvAxisAlignedBox      m_BoundingBox;
+    Vector<BvhNode>         m_Nodes;
+    Vector<unsigned int>    m_Indirection;
+    BvAxisAlignedBox        m_BoundingBox;
 };
+
+HK_FORCEINLINE BvhTree::BvhTree(BvhTree&& Rhs) noexcept :
+    m_Nodes(std::move(Rhs.m_Nodes)),
+    m_Indirection(std::move(Rhs.m_Indirection)),
+    m_BoundingBox(Rhs.m_BoundingBox)
+{
+    Rhs.m_BoundingBox.Clear();
+}
+
+HK_FORCEINLINE BvhTree& BvhTree::operator=(BvhTree&& Rhs) noexcept
+{
+    m_Nodes = std::move(Rhs.m_Nodes);
+    m_Indirection = std::move(Rhs.m_Indirection);
+    m_BoundingBox = Rhs.m_BoundingBox;
+    Rhs.m_BoundingBox.Clear();
+    return *this;
+}
+
+template <typename VertexType>
+HK_FORCEINLINE BvhTree::BvhTree(ArrayView<VertexType> Vertices, ArrayView<unsigned int> Indices, int BaseVertex, unsigned int PrimitivesPerLeaf) :
+    BvhTree(&Vertices[0].Position, Vertices.Size(), sizeof(VertexType), Indices, BaseVertex, PrimitivesPerLeaf)
+{}
+
+HK_FORCEINLINE bool BvhNode::IsLeaf() const
+{
+    return Index >= 0;
+}
+
+HK_INLINE void BvhNode::Read(IBinaryStreamReadInterface& Stream)
+{
+    Stream.ReadObject(Bounds);
+    Index          = Stream.ReadInt32();
+    PrimitiveCount = Stream.ReadInt32();
+}
+
+HK_INLINE void BvhNode::Write(IBinaryStreamWriteInterface& Stream) const
+{
+    Stream.WriteObject(Bounds);
+    Stream.WriteInt32(Index);
+    Stream.WriteInt32(PrimitiveCount);
+}
 
 HK_NAMESPACE_END
