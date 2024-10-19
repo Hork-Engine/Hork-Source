8b8736d54631ca3f61a9cc8a16d8295b16863922
Alexander Samusev <alsamusev@list.ru>
2024-10-19 12:40:31 +0300

    Formatting

--- Hork/Embedded/Shaders/base/core.glsl
+++ Hork/Embedded/Shaders/base/core.glsl
@@ -33,61 +33,68 @@ SOFTWARE.
 
 #define PI 3.1415926
 
-#define saturate( val ) clamp( val, 0.0, 1.0 )
-#define mad( a, b, c ) ((a)*(b)+(c))
+#define saturate(val) clamp(val, 0.0, 1.0)
+#define mad(a, b, c)  ((a) * (b) + (c))
 
-vec2 builtin_spheremap_coord( vec3 dir ) {
-  vec2 uv = vec2( atan( dir.z, dir.x ), asin( -dir.y ) );
-  return uv * vec2(0.1591, 0.3183) + 0.5;
+vec2 builtin_spheremap_coord(in vec3 dir)
+{
+    vec2 uv = vec2(atan(dir.z, dir.x), asin(-dir.y));
+    return uv * vec2(0.1591, 0.3183) + 0.5;
 }
 
-float builtin_luminance( vec3 color ) {
-  return dot( color, vec3( 0.2126, 0.7152, 0.0722 ) );
+float builtin_luminance(in vec3 color)
+{
+    return dot(color, vec3(0.2126, 0.7152, 0.0722));
 }
 
-float builtin_luminance( vec4 color ) {
-  return dot( color, vec4( 0.2126, 0.7152, 0.0722, 0.0 ) );
+float builtin_luminance(in vec4 color)
+{
+    return dot(color, vec4(0.2126, 0.7152, 0.0722, 0.0));
 }
 
-float log10( float x )
+float log10(float x)
 {
     const float base10 = 1.0 / log(10.0);
-    return log( x ) * base10;
+    return log(x) * base10;
 }
 
-vec2 log10( vec2 x )
+vec2 log10(vec2 x)
 {
     const float base10 = 1.0 / log(10.0);
-    return log( x ) * base10;
+    return log(x) * base10;
 }
 
-vec3 log10( vec3 x )
+vec3 log10(vec3 x)
 {
     const float base10 = 1.0 / log(10.0);
-    return log( x ) * base10;
+    return log(x) * base10;
 }
 
-vec4 log10( vec4 x )
+vec4 log10(vec4 x)
 {
     const float base10 = 1.0 / log(10.0);
-    return log( x ) * base10;
+    return log(x) * base10;
 }
 
-float rand( vec2 co ) {
-    return fract( sin( dot( co, vec2( 12.9898, 78.233 ) ) ) * 43758.5453 );
+float rand(vec2 co)
+{
+    return fract(sin(dot(co, vec2(12.9898, 78.233))) * 43758.5453);
 }
 
-float rand( vec4 co ) {
-    return fract( sin( dot( co, vec4( 12.9898, 78.233, 45.164, 94.673 ) ) ) * 43758.5453 );
+float rand(vec4 co)
+{
+    return fract(sin(dot(co, vec4(12.9898, 78.233, 45.164, 94.673))) * 43758.5453);
 }
 
-vec3 sepia( vec3 color, float scale ) {
-    const vec3 c = dot( color, vec3( 0.2126, 0.7152, 0.0722 ) ) * vec3(1.0,0.89,0.71);
-    return mix( color, c, scale );
+vec3 sepia(vec3 color, float scale)
+{
+    const vec3 c = dot(color, vec3(0.2126, 0.7152, 0.0722)) * vec3(1.0, 0.89, 0.71);
+    return mix(color, c, scale);
 }
 
-vec3 grayscale( vec3 color, float scale ) {
-    return mix( color, vec3( dot( color, vec3( 0.2126, 0.7152, 0.0722 ) ) ), scale );
+vec3 grayscale(vec3 color, float scale)
+{
+    return mix(color, vec3(dot(color, vec3(0.2126, 0.7152, 0.0722))), scale);
 }
 
 #endif // CORE_H
