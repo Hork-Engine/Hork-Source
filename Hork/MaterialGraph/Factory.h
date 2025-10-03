1bc4937e2a82443917320502366b043e44f507ab
Alexander Samusev <alsamusev@list.ru>
2025-10-03 14:29:10 +0300

    Bump license year

--- Hork/MaterialGraph/Factory.h
+++ Hork/MaterialGraph/Factory.h
@@ -4,7 +4,7 @@ Hork Engine Source Code
 
 MIT License
 
-Copyright (C) 2017-2024 Alexander Samusev.
+Copyright (C) 2017-2025 Alexander Samusev.
 
 This file is part of the Hork Engine Source Code.
 
